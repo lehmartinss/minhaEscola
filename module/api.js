1ff12cb492aa671375f5d7525ba56944bf51214a
LETICIA BEATRIZ MARTINS <24122309@dsn127m1249388.cfp127aluno.local>
2025-08-07 11:30:57 -0300

    back ok

--- module/api.js
+++
@@ -1,8 +0,0 @@
-'use scrict'
-
-export const getMinhaEscola = async () => {
-    const url = 'http://localhost:3000/fotos'
-    const response = await fetch(url)
-    const data = await response.json()
-    return data;
-}
